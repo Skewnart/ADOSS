168c9a257f1b2f5a0a53e7e5ea6bd61f95ced5aa
Skewnart <corentinz@outlook.fr>
2018-02-03 10:43:51 +0100

    Process command register and changepassword

--- Server/System/Command.cs
+++ Server/System/Command.cs
@@ -11,7 +11,7 @@ namespace Server.System
     {
         public static string ProcessCommand(Socket client, string command, CommandSource source)
         {
-            bool _logIt = false;
+            bool _logIt = true;
             string result = "601";
 
             string[] commands = command.Explode(' ', '"');
@@ -20,7 +20,7 @@ namespace Server.System
             {
                 if (source == CommandSource.CommandLine)
                 {
-
+                    _logIt = false;
                 }
                 else if (source == CommandSource.Socket)
                 {
@@ -30,37 +30,77 @@ namespace Server.System
                             $"$$603;;L'utilisateur n'existe pas.;;User does not exist." +
                             $"$$604;;Le service demandé n'existe pas.;;Requested service does not exist." +
                             $"$$605;;L'utilisateur n'a pas accès à ce service.;;Access is denied." +
-                            $"$$606;;L'accès est en cours d'acceptation.;;Access is still pending." +
-                            $"$$607;;Le mot de passe ne correspond pas.;;Password does not match." +
-                            $"$$608;;Accès autorisé.;;Access granted.";
+                            $"$$606;;Le service est en cours d'acceptation.;;Service is still pending." +
+                            $"$$607;;Le mot de passe n'est pas bon.;;Password incorrect." +
+                            $"$$608;;Accès autorisé.;;Access granted." +
+                            $"$$609;;L'utilisateur est déjà enregistré.;;User already registered." +
+                            $"$$610;;Le mot de passe n'a pas changé.;;Password didn't change." +
+                            $"$$611;;Le mot de passe a bien changé.;;Password successfully changed.";
                     else if (commands[0].Equals("user") && commands[1].Equals("connect"))
                     {
-                        if (commands.Length == 5)
-                            if (Store.Accesses.Any(x => x.Name.Equals(commands[2])))
-                                if (Store.Users.Any(x => x.Username.Equals(commands[3])))
-                                {
-                                    User user = Store.Users.First(x => x.Username.Equals(commands[3]));
-                                    if (user.Accesses.Any(x => x.Access.Name.Equals(commands[2])))
-                                    {
-                                        UserAccess access = user.Accesses.First(x => x.Access.Name.Equals(commands[2]));
-
-                                        if (access.Password.Decrypt().Equals(commands[4].Decrypt()))
-                                            result = $"608;;{GenerateToken(client, user, access.Access)}";
-                                        else
-                                            result = "607";
-                                    }
-                                    else
-                                        if (user.Pendings.Any(x => x.Access.Name.Equals(commands[2])))
-                                        result = "606";
-                                    else
-                                        result = "605";
-                                }
-                                else
-                                    result = "603";
-                            else
-                                result = "604";
+                        if (commands.Length != 5) result = "602";
+                        else if (Store.Accesses.TrueForAll(x => !x.Name.Equals(commands[2]))) result = "604";
+                        else if (Store.Users.TrueForAll(x => !x.Username.Equals(commands[3]))) result = "603";
                         else
-                            result = "602";
+                        {
+                            User user = Store.Users.First(x => x.Username.Equals(commands[3]));
+                            if (user.Accesses.Any(x => x.Access.Name.Equals(commands[2])))
+                            {
+                                UserAccess access = user.Accesses.First(x => x.Access.Name.Equals(commands[2]));
+
+                                if (access.Password.Decrypt().Equals(commands[4].Decrypt()))
+                                    result = $"608;;{GenerateToken(client, user, access.Access)}";
+                                else
+                                    result = "607";
+                            }
+                            else if (user.Pendings.Any(x => x.Access.Name.Equals(commands[2])))
+                                result = "606";
+                            else
+                                result = "605";
+                        }
+                    }
+                    else if (commands[0].Equals("user") && commands[1].Equals("register"))
+                    {
+                        if (commands.Length != 5) result = "602";
+                        else if (Store.Accesses.TrueForAll(x => !x.Name.Equals(commands[2]))) result = "604";
+                        else if (Store.Users.FirstOrDefault(x => x.Username.Equals(commands[3]))?.Accesses.Any(x => x.Access.Name.Equals(commands[2])) ?? false) result = "609";
+                        else if (Store.Users.FirstOrDefault(x => x.Username.Equals(commands[3]))?.Pendings.Any(x => x.Access.Name.Equals(commands[2])) ?? false) result = "606";
+                        else
+                        {
+                            if (!Store.Users.Any(x => x.Username.Equals(commands[3]))) Store.Users.Add(new User() { Username = commands[3] });
+                            User user = Store.Users.FirstOrDefault(x => x.Username.Equals(commands[3]));
+                            user.Pendings.Add(new UserAccess(Store.Accesses.FirstOrDefault(x => x.Name.Equals(commands[2])), commands[4]));
+                            result = "606";
+
+                            //Store.Users.Save();
+                        }
+                    }
+                    else if (commands[0].Equals("user") && commands[1].Equals("changepassword"))
+                    {
+                        if (commands.Length != 6) result = "602";
+                        else if (!Store.Accesses.Any(x => x.Name.Equals(commands[2]))) result = "604";
+                        else if (!Store.Users.Any(x => x.Username.Equals(commands[3]))) result = "603";
+                        else
+                        {
+                            User user = Store.Users.FirstOrDefault(x => x.Username.Equals(commands[3]));
+                            if (!user.Accesses.Any(x => x.Access.Name.Equals(commands[2])) && !user.Pendings.Any(x => x.Access.Name.Equals(commands[2]))) result = "605";
+                            else
+                            {
+                                UserAccess access = user.Accesses.Any(x => x.Access.Name.Equals(commands[2])) ? user.Accesses.FirstOrDefault(x => x.Access.Name.Equals(commands[2])) : user.Pendings.FirstOrDefault(x => x.Access.Name.Equals(commands[2]));
+                                if (!access.Password.Decrypt().Equals(commands[4].Decrypt())) result = "607";
+                                else if (commands[4].Decrypt().Equals(commands[5].Decrypt())) result = "610";
+                                else
+                                {
+                                    access.Password = commands[5];
+                                    result = "611";
+                                    //Store.Users.Save();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _logIt = false;
                     }
                 }
 
